2cfa852d5cee91b327f172c73e6d62794886533e
RaulMoya14 <115221547+RaulMoya14@users.noreply.github.com>
2023-12-13 16:50:59 +0100

    Update service.py

--- server/adiserver/service.py
+++ server/adiserver/service.py
@@ -14,7 +14,7 @@ class BlobDB:
     def __init__(self, db_file):
         self.db_file = db_file
         try:
-            self.conn = sqlite3.connect(db_file)
+            self.conn = sqlite3.connect(self.db_file, check_same_thread=False)
             print("Opened database successfully")
             self.conn.execute('''CREATE TABLE IF NOT EXISTS blobs
                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,7 +31,6 @@ class BlobDB:
 
     def create_blob(self, name, local_name, visibility, users,owner_blob):
         try:
-            self.conn = sqlite3.connect(self.db_file, check_same_thread=False)
             cursor = self.conn.cursor()  # Obtenemos un cursor en lugar de abrir una nueva conexión
             cursor.execute("INSERT INTO blobs (name, local_name, visibility, users, owner) VALUES (?, ?, ?, ?, ?)",
                         (name, local_name, visibility, users,owner_blob))
@@ -45,7 +44,6 @@ class BlobDB:
     def blobOwner(self, blobId):
 
         try:
-            self.conn = sqlite3.connect(self.db_file)
             cursor = self.conn.cursor()
             cursor.execute("SELECT owner FROM blobs WHERE id = ?", (blobId,))
             return cursor.fetchone()[0]
@@ -53,10 +51,8 @@ class BlobDB:
             print(f"Error al obtener el owner del blob: {str(e)}")
             return None
         
-    def getVisibilityBlob(self, blobId):
-            
+    def getVisibilityBlob(self, blobId):  
         try:
-            self.conn = sqlite3.connect(self.db_file)
             cursor = self.conn.cursor()
             cursor.execute("SELECT visibility FROM blobs WHERE id = ?", (blobId,))
             return cursor.fetchone()[0]
@@ -65,24 +61,18 @@ class BlobDB:
             return None
         
     def update_blob(self, id, name, local_name, visibility, users):
-        conn = None
         try:
             users_str = ', '.join(users)
-            conn = sqlite3.connect(self.db_file)
-            conn.execute(f"UPDATE blobs SET name = '{name}', local_name = '{local_name}', visibility = '{visibility}', users = '{users_str}' WHERE id = {id}")
-            conn.commit()
+            cursor = self.conn.cursor()
+            cursor.execute(f"UPDATE blobs SET name = '{name}', local_name = '{local_name}', visibility = '{visibility}', users = '{users_str}' WHERE id = {id}")
+            cursor.commit()
             print("Record updated successfully")
         except:
             print("Record update failed")
-        finally:
-            if conn:
-                conn.close()
 
     def delete_blob(self, id):
-        conn = None
         try:
-            conn = sqlite3.connect(self.db_file)
-            cursor = conn.execute(f"SELECT * FROM blobs WHERE id = {id}")
+            cursor = self.conn.execute(f"SELECT * FROM blobs WHERE id = {id}")
             if cursor.fetchone() is not None:
                 conn.execute(f"DELETE FROM blobs WHERE id = {id}")
                 conn.commit()
@@ -91,14 +81,10 @@ class BlobDB:
                 print("Record with ID {} not found".format(id))
         except sqlite3.Error as e:
             print("Record deletion failed:", str(e))
-        finally:
-            if conn:
-                conn.close()
 
     def change_visibility(self, blob_id, new_visibility):
     
         try:
-            self.conn = sqlite3.connect(self.db_file)
             cursor = self.conn.cursor()
             cursor.execute("UPDATE blobs SET visibility = ? WHERE id = ?", (new_visibility, blob_id))
             self.conn.commit()
@@ -108,8 +94,7 @@ class BlobDB:
     def get_blobs_by_user(self,user):
     
         try:
-            conn = sqlite3.connect(self.db_file)
-            cursor = conn.cursor()
+            cursor = self.conn.cursor()
             cursor.execute("SELECT id, name, local_name, visibility, users FROM blobs WHERE owner = ?", (user,))
 
             rows = cursor.fetchall()
@@ -132,10 +117,9 @@ class BlobDB:
             return []
 
     def get_blob(self, id):
-        conn = None
+        
         try:
-            conn = sqlite3.connect(self.db_file)
-            cursor = conn.execute(f"SELECT * FROM blobs WHERE id = {id}")
+            cursor = self.conn.execute(f"SELECT * FROM blobs WHERE id = {id}")
             row = cursor.fetchone()
             if row is not None:
                 print("Record selected successfully")
@@ -146,35 +130,24 @@ class BlobDB:
         except sqlite3.Error as e:
             print("Record selection failed:", str(e))
             return None
-        finally:
-            if conn:
-                conn.close()
 
     def get_users(self, id):
-        conn = None
         try:
-            conn = sqlite3.connect(self.db_file)
-            cursor = conn.execute(f"SELECT users FROM blobs WHERE id = {id}")
+            cursor = self.conn.execute(f"SELECT users FROM blobs WHERE id = {id}")
             return cursor.fetchone()
         except:
             print("Record selection failed")
-        finally:
-            if conn:
-                conn.close()
+            return None
 
     def delete_user(self,blobId,user_to_remove_permission):
-        conn = None
+        
         try:
-            conn = sqlite3.connect(self.db_file)
-            cursor = conn.execute(f"SELECT users FROM blobs WHERE id = {blobId}")
+            cursor = self.conn.execute(f"SELECT users FROM blobs WHERE id = {blobId}")
             users = cursor.fetchone()[0]
             users_list = users.split(",")
             users_list.remove(user_to_remove_permission)
             users = ",".join(users_list)
-            conn.execute(f"UPDATE blobs SET users = '{users}' WHERE id = {blobId}")
-            conn.commit()
+            self.conn.execute(f"UPDATE blobs SET users = '{users}' WHERE id = {blobId}")
+            self.conn.commit()
         except:
             print("Record update failed")
-        finally:
-            if conn:
-                conn.close()
