6d18983cb3df389d627cb9a830abfe94cd3cf160
RaulMoya14 <115221547+RaulMoya14@users.noreply.github.com>
2023-12-17 18:19:06 +0100

    Update build_vagrant.sh

--- build_vagrant.sh
+++ build_vagrant.sh
@@ -12,8 +12,17 @@ while [[ $# -gt 0 ]]; do
       # Parámetros adicionales de configuración (ajusta según tus necesidades)
       qcow2_options="-f qcow2 -o preallocation=metadata"
 
+      # Crear una nueva imagen qcow2 y copiar el contenido de la imagen de VirtualBox
+      qemu-img create $qcow2_options $qcow2_image_name 10G
+      sudo modprobe nbd max_part=16
+      sudo qemu-nbd -c /dev/nbd0 $qcow2_image_name
+      sudo dd if=output.box of=/dev/nbd0 bs=16M status=progress
+      sudo qemu-nbd -d /dev/nbd0
+      sudo modprobe -r nbd
       # Ejecutar qemu-img para convertir la imagen VDI de VirtualBox a QCOW2
-      qemu-img convert -O $qcow2_options output.box $qcow2_image_name
+      qemu-img convert -O $qcow2_options output.box "$PWD/$qcow2_image_name"
+
+      echo "QCOW2 generado: $PWD/$qcow2_image_name"
 
       echo "QCOW2 generado: $qcow2_image_name"
       shift
